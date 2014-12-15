70cede6e5955f9ac672c0cd6af33b22a4c2e7375
greatjin <nicejin74@gmail.com>
2014-12-15 16:17:32 +0900

    dd

--- HelloPython/test/picamera.py
+++ HelloPython/test/picamera.py
@@ -4,16 +4,31 @@ Created on 2014. 12. 15.
 @author: Administrator
 '''
 
-import picamera
+'''import picamera
 import time
-
+'''
 '''camera = picamera.PiCamera()
 camera.vflip=True
 camera.capture('image.jpg')
 '''
-with picamera.PiCamera() as camera:
+
+'''with picamera.PiCamera() as camera:
     camera.resolution = (1024, 768)
     camera.start_preview()
     # Camera warm-up time
     time.sleep(2)
-    camera.capture('foo.jpg')
+    camera.capture('foo.jpg')'''
+    
+    
+import time
+import picamera
+with picamera.PiCamera() as camera:
+    camera.start_preview()
+    try:
+        for i, filename in enumerate(camera.capture_continuous('image{counter:02d}.jpg')):
+            print(filename)
+            time.sleep(1)
+            if i == 59:
+                break
+    finally:
+        camera.stop_preview()    
